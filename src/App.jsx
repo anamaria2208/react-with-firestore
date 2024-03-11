9f2301a09b9855f944b6b68fae9d4719a9c91f02
Ana-Maria <anamariaolujic2208@gmail.com>
2024-03-11 18:29:01 +0100

    added delete function and real time fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,60 @@
-import { useState } from 'react'
-import { getBooks } from './api/firebase_api'
+import { useState, useRef, useEffect } from "react";
+import { getBooks, postBooks, deleteBook } from "./api/firebase_api";
 
 function App() {
+  const [books, setBooks] = useState(null);
+  const authorRef = useRef(null);
+  const titleRef = useRef(null);
 
-  const [books, setBooks] = useState(null)
-  const handleGetBooks = async() => {
-    let response = await getBooks();
-    setBooks(response)
-  }
+  useEffect(() => {
+    const unsubscribe = getBooks((newBooks) => {
+      setBooks(newBooks)
+    })
+    return () => unsubscribe()
+  }, []);
+
+  const handleDelete = async (id) => {
+    await deleteBook(id);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const authorValue = authorRef.current.value;
+    const titleValue = titleRef.current.value;
+    await postBooks(titleValue, authorValue);
+
+    authorRef.current.value = "";
+    titleRef.current.value = "";
+  };
 
   return (
     <>
       <h1>Books store</h1>
-      <button onClick={handleGetBooks}>Get Books</button>
       <ul>
-        {books &&  books.map(book => <li key={book.id}>{book.Author} + {book.Title}</li>)}
+        {books &&
+          books.map((book) => (
+            <li key={book.id}>
+              {book.author} + {book.title}
+              <button onClick={() => handleDelete(book.id)}>Delete</button>
+            </li>
+          ))}
       </ul>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="author">Author:</label>
+          <input type="text" ref={authorRef} />
+        </div>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input type="text" ref={titleRef} />
+        </div>
+        <div>
+          <button type="submit">Add book</button>
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/api/firebase_api.js
+++ src/api/firebase_api.js
@@ -1,15 +1,42 @@
 // third party library
-import { collection, getDocs } from "firebase/firestore";
+import { collection, onSnapshot , addDoc, serverTimestamp, doc, deleteDoc, getDocs  } from "firebase/firestore";
 
 // local files
 import { db } from "../firebase";
 
+// BOOK REF COL
+const bookColRef = collection(db, "Books");
+
 // GET BOOKS API
-export const getBooks = async () => {
-  const querySnapshot = await getDocs(collection(db, "Books"));
-  const docArray = []
-  querySnapshot.forEach(doc => {
-    docArray.push({...doc.data(), id:doc.id})
+// export const getBooks = async () => {
+//   const querySnapshot = await getDocs(bookColRef);
+//   const docArray = []
+//   querySnapshot.forEach(doc => {
+//     docArray.push({...doc.data(), id:doc.id})
+//   })
+//   return docArray;
+// };
+
+// REAL TIME DATA
+export const  getBooks = (cb) => {
+  return onSnapshot(bookColRef, snapshot => {
+    const updatedBooks = []
+    snapshot.forEach(doc => {
+      updatedBooks.push({id : doc.id, ...doc.data()})
+    })
+    cb(updatedBooks)
   })
-  return docArray;
-};
+}
+
+// POST BOOKS API
+export const postBooks = async (title, author) => {
+  const bookRef = await addDoc(bookColRef, {
+    title : title, author : author, timestamp: serverTimestamp()
+  })
+  return bookRef.id
+}
+
+// DELETE BOOKS API
+export const deleteBook = async (id) => {
+  await deleteDoc(doc(db, "Books", id))
+}
